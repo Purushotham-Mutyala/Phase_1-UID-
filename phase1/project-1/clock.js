86ddaa5dcf718af84aa4cdbd761f72cc2905f818
Purushotham-Mutyala <uthammutyala@gmail.com>
2025-04-06 23:38:34 +0530

    done

--- phase1/project-1/clock.js
+++
@@ -1,7 +0,0 @@
-function updateClock() {
-    const now = new Date();
-    document.getElementById('time').textContent = now.toLocaleTimeString();
-    document.getElementById('date').textContent = now.toLocaleDateString();
-  }
-  setInterval(updateClock, 1000);
-  updateClock();
